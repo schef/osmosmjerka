ae09389ffe12e6b7b024619ae2a70afc358ca5bd
schef <zvanstefan@gmail.com>
2019-02-27 08:02:16 +0100

    init start

---
+++ src/colors.h
@@ -0,0 +1,12 @@
+#ifndef __COLORS_H__
+#define __COLORS_H__
+
+#define CF_RED     "\x1b[31m"
+#define CF_GREEN   "\x1b[32m"
+#define CF_YELLOW  "\x1b[33m"
+#define CF_BLUE    "\x1b[34m"
+#define CF_MAGENTA "\x1b[35m"
+#define CF_CYAN    "\x1b[36m"
+#define CF_RESET   "\x1b[0m"
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "colors.h"
+
+#define FILE_NAME CF_RED "main: " CF_RESET
+
+
+
+int main() {
+    printf(FILE_NAME "boot sequence start\n");
+    printf(FILE_NAME "boot sequence end\n");
+    return 0;
+}
